46d45b5c0578ec05158dd05944d5974bd5d33b3b
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-08-09 17:08:14 -0300

    add DTO to list all Categorias

--- src/main/java/com/leodev0/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/leodev0/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.leodev0.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leodev0.cursomc.domain.Categoria;
+import com.leodev0.cursomc.dto.CategoriaDTO;
 import com.leodev0.cursomc.services.CategoriaService;
 
 @RestController
@@ -49,5 +52,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream()
+				.map(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList());  // essa linha é responsável por transformar esse stream de objetos de volta para o tipo lista.
+		return ResponseEntity.ok().body(listDto);
 	}
 }
