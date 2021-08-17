1cdb49bdf19b6a6dfbecb5d5f9c3d7e2a83d29a7
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-08-17 17:15:43 -0300

    add endpoint to search products by nome and categorias

--- src/main/java/com/leodev0/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/leodev0/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,27 @@
 package com.leodev0.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.leodev0.cursomc.domain.Categoria;
 import com.leodev0.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+//	@Transactional(readOnly=true)
+//	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+//	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
+//	DOCUMENTAÇÃO DESSE TIPO DE MÉTODO: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+		
 }
