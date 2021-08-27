0cf6545456cb5728243cfbc594a7236ff11894ce
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-08-27 17:15:05 -0300

    add user profile (Perfil enum) to Cliente entity

--- src/main/java/com/leodev0/cursomc/services/DBService.java
+++ src/main/java/com/leodev0/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.leodev0.cursomc.domain.PagamentoComCartao;
 import com.leodev0.cursomc.domain.Pedido;
 import com.leodev0.cursomc.domain.Produto;
 import com.leodev0.cursomc.domain.enums.EstadoPagamento;
+import com.leodev0.cursomc.domain.enums.Perfil;
 import com.leodev0.cursomc.domain.enums.TipoCliente;
 import com.leodev0.cursomc.repositories.CategoriaRepository;
 import com.leodev0.cursomc.repositories.CidadeRepository;
@@ -120,16 +121,21 @@ public class DBService {
 		
 //		Para o envio de email funcionar trocar o email 'maria@gmail.com' por um gmail válida, e também colocar as credenciais gmail em 'application-dev.properties'
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838392"));
+		
+		Cliente cli2 = new Cliente(null, "Ana costa", "ana@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 //		----------------------------
 		
