44f2957a51acbbe07e2de196120c59de7c7fae94
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-09-12 15:52:03 -0300

    add file upload by the endpoint '/clientes/picure'

--- src/main/java/com/leodev0/cursomc/CursomcApplication.java
+++ src/main/java/com/leodev0/cursomc/CursomcApplication.java
@@ -1,17 +1,12 @@
 package com.leodev0.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.leodev0.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	@Autowired
-	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,8 +14,5 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		s3service.uploadFile("/home/kb37/Imagens/0.jpeg");
-		
 	}
 }
